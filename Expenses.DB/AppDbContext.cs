8d4075ecc8b2582d47fc045a93575ec0a865b765
Alain Nshimirimana <alain.nshimirimana@gmail.com>
2021-03-10 17:12:01 -0800

    Added DbContext and created the initial migration

---
+++ Expenses.DB/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Expenses.DB
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=ExpensesDb;Integrated Security=True");
+        }
+    }
+}
